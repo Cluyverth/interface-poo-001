fbd58268e664fb7086524601a2640044b25aeb01
Cluyverth W. <ContatoCluyverth@gmail.com>
2024-12-28 17:46:40 -0300

    organize the files

---
+++ ProjectC/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+Console.WriteLine("Enter contract data");
+Console.WriteLine("Number: ");
+int contractNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Date (dd/MM/yyyy): ");
+DateTime endDate = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
+Console.WriteLine("Contract Value: ");
+double contractValue = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter The Number of Installments: ");
+int contractInstallmentsNumber = int.Parse(Console.ReadLine());
+
